183037f9433ef5ebaca23b733c8328797e9df9a6
Rahmat Triadi <rahmat_triadi@yahoo.com>
2020-08-02 22:23:41 +0800

    cuman ba edit tampilan lo approve spd

--- application/views/pegawai/spdku/approve_spdku.php
+++ application/views/pegawai/spdku/approve_spdku.php
@@ -6,12 +6,12 @@
 		<div class="container-fluid">
 			<div class="row mb-2">
 				<div class="col-sm-6">
-					<h1 class="m-0 text-dark">SPD</h1>
+					<h1 class="m-0 text-dark">Approve SPD</h1>
 				</div><!-- /.col -->
 				<div class="col-sm-6">
 					<ol class="breadcrumb float-sm-right">
 						<li class="breadcrumb-item"><a href="<?= site_url('dashboard') ?>">Home</a></li>
-						<li class="breadcrumb-item active">SPD</li>
+						<li class="breadcrumb-item active">Approve SPD</li>
 					</ol>
 				</div><!-- /.col -->
 			</div><!-- /.row -->
@@ -27,7 +27,7 @@
 				<div class="col-lg-12">
 					<div class="card">
 						<div class="card-header">
-							<h3 class="card-title">SPD-KU</h3>
+							<h3 class="card-title">Approve SPD</h3>
 						</div>
 						<!-- /.card-header -->
 						<div class="card-body table-responsive">
